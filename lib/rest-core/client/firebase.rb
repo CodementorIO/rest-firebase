8f215e44c994772c50390969592f8e7d3d90034d
Lin Jen-Shin <godfat@godfat.org>
2014-05-03 06:53:42 +0800

    a firebase client! TODO: error handling

---
+++ lib/rest-core/client/firebase.rb
@@ -0,0 +1,50 @@
+
+require 'rest-core'
+
+# https://www.firebase.com/docs/security/custom-login.html
+# https://www.firebase.com/docs/rest-api.html
+module RestCore
+  Firebase = Builder.client(:d, :secret, :auth) do
+    use Timeout       , 10
+
+    use DefaultSite   , 'https://SampleChat.firebaseIO-demo.com/'
+    use DefaultHeaders, {'Accept' => 'application/json'}
+    use DefaultQuery  , nil
+
+    use CommonLogger  , nil
+    use Cache         , nil, 600 do
+      use ErrorHandler, lambda{ |env|
+        RuntimeError.new(env[RESPONSE_BODY]['message'])}
+      use ErrorDetectorHttp
+      use JsonResponse, true
+    end
+  end
+end
+
+module RestCore::Firebase::Client
+  include RestCore
+
+  def generate_auth opts={}
+    header = {:typ => 'JWT', :alg => 'HS256'}
+    claims = {:v => 0, :iat => Time.now.to_i, :d => d}.merge(opts)
+    # http://tools.ietf.org/html/draft-ietf-jose-json-web-signature-26
+    input = [header, claims].map{ |d| base64url(Json.encode(d)) }.join('.')
+    # http://tools.ietf.org/html/draft-ietf-oauth-json-web-token-20
+    "#{input}.#{base64url(Hmac.sha256(secret, input))}"
+  end
+
+  def request env, key=RESPONSE_BODY, app=app
+    super(env.merge(REQUEST_PATH    => "#{env[REQUEST_PATH]}.json",
+                    REQUEST_PAYLOAD => Json.encode(env[REQUEST_PAYLOAD])),
+          key, app)
+  end
+
+  private
+  def base64url str; [str].pack('m').tr('+/', '-_'); end
+  def default_query; {:auth => auth}; end
+  def default_auth ; generate_auth  ; end
+end
+
+class RestCore::Firebase
+  include RestCore::Firebase::Client
+end
